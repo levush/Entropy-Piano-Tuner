2a5d812fe872ef941d3c3c0b7c88ac7deb6d59e7
C. Wick <cwick@uni-wuerzburg.de>
2015-06-11 16:50:00 +0200

    added upload script for MacOsX

---
+++ scripts/loadVersion.sh
@@ -0,0 +1,42 @@
+set -e
+
+# read version from version.h
+versionfile="../core/system/version.h"
+if [ -f "$versionfile" ]
+then
+	echo "$versionfile found."
+else
+	echo "$versionfile not found."
+	exit 1
+fi
+
+versioncontents=`cat $versionfile`
+releaseDate=$(date +"%Y-%m-%d")
+
+reVersionString="EPT_VERSION_STRING[[:space:]]+\"([0-9\.]+)\""
+reVersionRolling="EPT_VERSION_ROLLING[[:space:]]+([0-9]+)"
+
+reDepsVersionString="EPT_DEPS_VERSION_STRING[[:space:]]+\"([0-9\.]+)\""
+reDepsVersionRolling="EPT_DEPS_VERSION_ROLLING[[:space:]]+([0-9]+)"
+
+
+if [[ $versioncontents =~ $reVersionString ]]; then
+    export versionString=${BASH_REMATCH[1]}
+    echo "Detected version: $versionString"
+fi
+
+if [[ $versioncontents =~ $reVersionRolling ]]; then
+    export versionRolling=${BASH_REMATCH[1]}
+    echo "Detected rolling version: $versionRolling"
+fi
+
+
+if [[ $versioncontents =~ $reDepsVersionString ]]; then
+	export depsVersionString=${BASH_REMATCH[1]}
+	echo "Detected dependencies version: $depsVersionString"
+fi
+
+if [[ $versioncontents =~ $reDepsVersionRolling ]]; then
+	export depsVersionRolling=${BASH_REMATCH[1]}
+	echo "Detected dependencies rolling version: $depsVersionRolling"
+fi
