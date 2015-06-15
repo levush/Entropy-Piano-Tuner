cdb4d1fbb4a10031e9ab7fdbb386cf9b301e2d7f
C. Wick <cwick@uni-wuerzburg.de>
2015-06-15 12:43:59 +0200

    reworked mac build procedure and added MAC_README

---
+++ scripts/mac/macMake.sh
@@ -0,0 +1,119 @@
+###########################################################
+# This file will handle the creation of compiling and
+# creating the mac dmg file
+###########################################################
+
+# automatic fail on errors
+set -e
+
+cd ${0%/*}
+
+# load the user environment
+. ./mac_env.user.sh
+
+DO_CLEAR=false
+DO_BUILD=false
+DO_DMG=false
+DO_UPLOAD=false
+DO_TRANSLATIONS=false
+
+echo "Parsing options."
+# parse options
+# -c clear
+# -b compile and build binary
+# -d make the dmg file
+# -u upload the files to the server
+# -t release translations
+while getopts ":cbdut" opt; do
+	case $opt in
+		c)
+			echo "Clearing build at $BUILD_DIR."
+			DO_CLEAR=true
+			;;
+		b) 
+			echo "Building."
+			DO_BUILD=true
+			;;
+		d)
+			echo "Creating dmg file."
+			DO_DMG=true
+			;;
+		u)
+			echo "Uploading to webserver."
+			DO_UPLOAD=true
+			;;
+		t)
+			echo "Releasing translations."
+			DO_TRANSLATIONS=true
+			;;
+		\?)
+			echo "Invalid options: -$OPTARG" >&2
+			exit -1
+			;;	
+	esac
+done
+
+echo "Options parsed."
+
+# Clear
+###########################################################
+if $DO_CLEAR ; then
+	echo "Clearing build at $BUILD_DIR."
+	rm -rf $BUILD_DIR
+
+	if $DO_DMG ; then
+		echo "Removing old dmg at $DMG_FILE."
+		rm -f $DMG_FILE
+	fi
+
+	echo "Done."
+fi
+
+# Release translations
+###########################################################
+if $DO_TRANSLATIONS ; then
+	echo "lrelease translations."
+	cd $TUNER_BASE_DIR/translations
+	for f in *.ts; do lrelease $f; done
+	echo "Done."
+fi
+
+# building
+###########################################################
+if $DO_BUILD ; then
+	echo "Building."
+	echo "Creating temporary build directory"
+	mkdir -p $BUILD_DIR
+	cd $BUILD_DIR
+	rm -rf $BINARY_FILE_NAME.app
+	qmake $TUNER_BASE_DIR/entropytuner.pro -r -spec macx-clang CONFIG+=$CONFIG
+	make $MAKE_ARGS
+
+	# copy info.plist and icns files on our own. there is a bug in qt
+	cp ../platforms/osx/info.plist $BINARY_FILE_NAME.app/Contents/.
+	cp ../appstore/icons/entropytuner.icns $BINARY_FILE_NAME.app/Contents/Resources/.
+
+	echo "Done."
+fi
+
+# create dmg
+###########################################################
+if $DO_DMG ; then
+	echo "Creating dmg."
+	cd $BUILD_DIR	
+	macdeployqt $BINARY_FILE_NAME.app -dmg
+	mv $BINARY_FILE_NAME.dmg ${DMG_FILE}
+	echo "Done."
+fi
+
+# upload to webserver
+###########################################################
+if $DO_UPLOAD ; then
+	echo "Uploading file."
+	. $SCRIPT_DIR/macUpload.sh
+	echo "Done."
+fi
+
+
+
+
