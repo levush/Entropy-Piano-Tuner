5d1d5397dffafd6093905e3d915af5395b97781d
C. Wick <ch.wick@web.de>
2015-07-17 12:59:58 +0200

    fixed typo

--- core/system/serverinfo.h
+++ core/system/serverinfo.h
@@ -9,7 +9,7 @@ namespace serverinfo
 static const std::string SERVER_DOMAIN                 = "piano-tuner.org";
 static const std::string SERVER_ADDRESS                = "http://" + SERVER_DOMAIN;
 static const std::string SERVER_NAME                   = "www." + SERVER_DOMAIN;
-static const std::string SERVER_DOWNLOADS_ADDRESS      = "http://donwload." + SERVER_DOMAIN + "/Resources/Public/Downloads";
+static const std::string SERVER_DOWNLOADS_ADDRESS      = "http://download." + SERVER_DOMAIN + "/Resources/Public/Downloads";
 static const std::string SERVER_DOCUMENTATION_ADDRESS  = "http://download." + SERVER_DOMAIN + "/Resources/Public/Documentation";
 static const std::string MAIL_ADDRESS                  = "info@" + SERVER_DOMAIN;
 
