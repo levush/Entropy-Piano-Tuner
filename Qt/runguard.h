8a003f6de6c7ae7e21e8abee56f13f1a43d15401
Haye Hinrichsen <hinrichsen@familie.fritz.box>
2015-08-16 08:33:20 +0200

    Doxygen for runguard added

--- Qt/runguard.h
+++ Qt/runguard.h
@@ -24,26 +24,36 @@
 #include <QSharedMemory>
 #include <QSystemSemaphore>
 
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Class for checking whether the application is already running
+///
+/// An object of this class can be used to prevent the system from starting
+/// serveral instances of the application. There should be no more than
+/// one open window of the EPT at a given time.
+///
+/// Internally the instances communicate via a QSharedMemory labelled by a
+/// given key. The access to this memory is mutexted by a QSystemSemaphore.
+///////////////////////////////////////////////////////////////////////////////
 
 class RunGuard
 {
-
 public:
-    RunGuard( const QString& key );
-    ~RunGuard();
+    RunGuard (const QString& key);
+    ~RunGuard() {release();};
 
     bool isAnotherRunning();
     bool tryToRun();
     void release();
 
 private:
-    const QString key;
-    const QString memLockKey;
-    const QString sharedmemKey;
+    QString generateKeyHash (const QString& key, const QString& salt);
 
-    QSharedMemory sharedMem;
-    QSystemSemaphore memLock;
+    const QString key;          ///< Name assigned to the instance
+    const QString memLockKey;   ///< Key labelling the semaphore lock
+    const QString sharedmemKey; ///< Key labelling the shared memory
 
+    QSharedMemory sharedMem;    ///< Shared memory labelled by a given key
+    QSystemSemaphore memLock;   ///< Lock used when shared memory is accessed
 };
 
 #endif  // RUN_GUARD
--- core/analyzers/overpull.h
+++ core/analyzers/overpull.h
@@ -25,26 +25,28 @@
 #define OVERPULL_H
 
 #include "../piano/piano.h"
-#include "../system/log.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 /// \brief Class for estimating the overpull needed in a pitch raise.
 ///
 /// If a piano is heavily out of tune, one has to perform a pitch raise.
-/// This process increases the load on the soundboard, leading to an
-/// elastic deformation of the soundboard. This deformation in turn lowers
-/// the pitches. Overpulling means to tune the strings a little bit higher
-/// than intended so that the loss during the process is compensated.
+/// The rising string tension increases the load on the soundboard, leading to
+/// an elastic deformation of the soundboard. This deformation in turn lowers
+/// the pitches. Overpulling means to tune the strings a little bit sharper
+/// than intended so that the expected loss during the tuning process is
+/// compensated.
 ///
 /// The overpull algorithm is implemented in a fully automatic manner. If in
 /// the recording mode enough keys are measured (all with a distance less or
 /// equal than a fifth), and if the piano is flat (or sharp) by more than
-/// 5%, the overpull curve is automatically shown in the tuning window.
+/// 5%, the overpull curve is automatically displayed in magenta color in
+/// the tuning window.
 ///
-/// The overpull decreases during tuning and finally vanishes. That is, for
-/// each key the overpull of all other keys is newly calculated.
+/// The overpull curve decreases during tuning and finally becomes flat and
+/// vanishes entirely. That is, for each key the overpull of all other keys
+/// is newly calculated.
 ///
-/// The instance of the overpull estimator is hold by the SignalAnalyzer.
+/// The instance of the overpull estimator is held by the SignalAnalyzer.
 ///////////////////////////////////////////////////////////////////////////////
 
 class OverpullEstimator
@@ -57,11 +59,11 @@ public:
     double getOverpull (int keynumber, const Piano *piano);
 
 private:
-    piano::PianoType mPianoType;
-    int mNumberOfKeys;
-    int mNumberOfBassKeys;
-    double mConcertPitch;
-    std::vector<std::vector<float>> R;
+    piano::PianoType mPianoType;        ///< Piano type (upright/grand)
+    int mNumberOfKeys;                  ///< Total number of keys
+    int mNumberOfBassKeys;              ///< Keys on the bass bridge
+    double mConcertPitch;               ///< Concert pitch (A4)
+    std::vector<std::vector<float>> R;  ///< Response matrix
 
     void computeInteractionMatrix (double averagePull = 0.22);
 };
